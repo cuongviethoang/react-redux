1c44f040fbca1693118637adbdaca0307d0cde91
cuongviethoang <hoangvietcuong2k2@gmail.com>
2024-03-03 23:18:05 +0700

    Cài đặt middleware redux-thunk

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import logo from "./logo.svg";
 import { connect } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./action/actions";
 // import store from "./redux/store";
-import axios from "axios";
 
 import Home from "./components/Home";
 
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+
 function App(props) {
     const dispatch = useDispatch();
 
@@ -25,15 +24,6 @@ function App(props) {
         dispatch(increaseCounter());
     };
 
-    const fetchAllUser = async () => {
-        const res = await axios.get("http://localhost:8080/api/v1/user/read");
-        const data = res && res.data ? res.data : [];
-        console.log(">> check data: ", data);
-    };
-
-    useEffect(() => {
-        fetchAllUser();
-    }, []);
     return <Home />;
 }
 
--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,6 +1,20 @@
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
+import axios from "axios";
+import { useEffect, useState } from "react";
 const TableUser = () => {
+    const [listUser, setListUser] = useState([]);
+    const fetchAllUser = async () => {
+        const res = await axios.get("http://localhost:8080/api/v1/user/read");
+        const data = res && res.data ? res.data.DT : [];
+        setListUser(data);
+    };
+
+    useEffect(() => {
+        fetchAllUser();
+    }, []);
+
+    const handleDeleteUser = (user) => {};
     return (
         <>
             <Container>
@@ -9,30 +23,31 @@ const TableUser = () => {
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
+                            <th>Email</th>
                             <th>Username</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Larry the Bird</td>
-                            <td>Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
+                        {listUser &&
+                            listUser.length > 0 &&
+                            listUser.map((user, index) => (
+                                <tr key={`user-${index + 1}`}>
+                                    <td>{index + 1}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.username}</td>
+                                    <td>
+                                        <button
+                                            className="btn btn-danger"
+                                            onClick={() =>
+                                                handleDeleteUser(user)
+                                            }
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
                     </tbody>
                 </Table>
             </Container>
